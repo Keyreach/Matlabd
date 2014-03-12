b51672f4aaae797066b1739edea0b38f7b626e82
Keyreach <anikinkirill94@yandex.ru>
2014-03-12 23:06:56 +0600

    Now it works

--- langranj.m
+++ langranj.m
@@ -12,12 +12,11 @@ k1 = (((x-x0)*(x-x2)*(x-x3))/((x1-x0)*(x1-x2)*(x1-x3)))*y1;
 k2 = (((x-x0)*(x-x1)*(x-x3))/((x2-x0)*(x2-x1)*(x2-x3)))*y2;
 k3 = (((x-x0)*(x-x1)*(x-x2))/((x3-x0)*(x3-x1)*(x3-x2)))*y3;
 f = simplify(k0+k1+k2+k3)
-subplot(1,1);
 ezplot(f);
-c0 = -1;
-c1 = 6;
-c2 = 2;
-c3 = 1/3;
+syms c3;
+c0 = y0;
+c1 = (y1-c0)/(x1-x0);
+c2 = (y2-c0-c1*(x2-x0))/((x2-x0)*(x2-x1));
+c3 = 1;
 f2 = simplify(c0+c1*(x-x0)+c2*(x-x0)*(x-x1)+c3*(x-x0)*(x-x1)*(x-x2))
-subplot(1,2);
 ezplot(f2);
